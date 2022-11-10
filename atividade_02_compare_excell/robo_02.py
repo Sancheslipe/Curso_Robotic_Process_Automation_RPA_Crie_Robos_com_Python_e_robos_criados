2e0b958138c810d170b883fddff269f89240ef04
Felipe Sanches <fsanches.0502@gmail.com>
2022-11-10 16:29:17 -0300

    bot_atividade_001 and bot_atividade_001 finished
    today i have finished :
    the bot_atividade_001 and
    the bot_atividade_002
    
    the twice robot is running and are following PEP08 and in 2 separated funcions:

---
+++ atividade_02_compare_excell/robo_02.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+def abrir_arquivos_e_separar():
+    #le o aquivo geral
+    arquivo_principal_df = pd.read_excel(f'C:\\Curso02_github\\atividade_02_compare_excell\\base_de_dados\\Arquivo_Principal.xlsx')
+    nomes_principal_df = arquivo_principal_df['nome']
+    nomes_totais_df = []
+    #caso a colunapossui nao exista ele cria e adiciona caracteres vázios
+    if 'possui' not in arquivo_principal_df:
+        arquivo_principal_df['possui'] = np.nan
+
+    #apende todos os arquivos em  so para fazer as comparacoes
+    for x in range(1,11):
+        nomes_df = pd.read_excel(f'C:\\Curso02_github\\atividade_02_compare_excell\\base_de_dados\\arquivo_{x}.xlsx')
+        nomes_totais_df.append(nomes_df['nome'])
+    print(f'a quantidade de linhas do arquivo principal é {len(nomes_principal_df)}\n')
+    return nomes_principal_df,nomes_totais_df,arquivo_principal_df
+
+
+def verifica_e_adiciona_no_arquivo(nomes_principal_df,nomes_totais_df,arquivo_principal_df):
+    #conta quantos numeros estao em mais de um excell
+    qtdsim = 0
+    #verifica se um nome do excell geral aparece em algum excel alem dele, caso apareca o campo possui = 'Sim, caso nao apareca o campo possui = 'Não'
+    for row in range(len(nomes_principal_df)):
+        if str(nomes_principal_df[row]) in str(nomes_totais_df):
+            qtdsim +=1
+            arquivo_principal_df.loc[row, 'possui'] = 'Sim'
+        else:
+            arquivo_principal_df.loc[row, 'possui'] = 'Não'
+    #printa a quantidade de nomes que aparecem mais de 1 vez
+    print(f'a quantidade de nomes que está em mais de 1 planilha é {qtdsim}\n')
+    #salva as alteracoes no arquivo geral
+    arquivo_principal_df.to_excel(f'C:\\Curso02_github\\atividade_02_compare_excell\\base_de_dados\\Arquivo_Principal.xlsx',index=False)
+
+  
+def bot_atividade_002():
+    try:
+        nome1,total_nome,arquivo_geral = abrir_arquivos_e_separar()
+        verifica_e_adiciona_no_arquivo(nome1,total_nome,arquivo_geral)
+        #if success:
+            #mandar email SUCESSO 
+    except:
+        exc_type, error, line = sys.exc_info()
+        print(f'ERROR: {error}\nCLASS: {exc_type}\nFUNC: {sys._getframe().f_code.co_name}\nLINE:  {line.tb_lineno}\n')    
+        # mandar email com o erro => nós 
+
+
+if (__name__ == '__main__'):
+    os.system('cls')
+    bot_atividade_002()
+    print('processo Encerrado !')
+
+
+'''
+Anotações e dicas de programas 
+Luiz. 
+
+#     # #baixa arquivo
+#     # #se baixou:
+#     # #editar
+#     #     df.loc[row, 'possui'] = 'OK'
+#     # #else:
+#     #     df.loc[row, 'possui'] = 'NOK'
+
+# if 'PROC' not in df.columns:
+#     df['PROC'] = 'NOK
+#     '
+
+
+
+# print(os.listdir(r'C:\Curso02_github\atividade_02_compare_excell\base_de_dados'))
+
+# excel_em_bs = [arq for arq in os.listdir(r'C:\Curso02_github\atividade_02_compare_excell\base_de_dados') if arq.endswith('xlsx')]
+'''
--- desafio_Final/desafio_luciano.py
+++ desafio_Final/desafio_luciano.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyautogui as p 
-import os as o 
+import os 
+import os.path
 import sys
 
 
@@ -30,15 +31,18 @@ def abrir_desafio_e_ler_planilha():
         p.click(download_planilha)
         p.sleep(2)
 
+        c = 0
         fechar_popUp_Download = None
-        while (fechar_popUp_Download is None):
+        while (fechar_popUp_Download is None) and (c < 15):
+            p.sleep(1)
+            c += 1
             fechar_popUp_Download = p.locateCenterOnScreen('C:\\Curso02_github\\desafio_Final\\fechar_Download_planilha.png')
             if (fechar_popUp_Download is not None):
                 p.click(fechar_popUp_Download)
                 p.sleep(1)
                 print('baixou arquivo com sucesso')
-                break
-            p.sleep(1)
+                if (c >= 2):
+                    break
 
         # Abrir Excel
         dados = pd.read_excel("C:\\Users\\Ana Paula\\Downloads\\challenge.xlsx", sheet_name='Sheet1')
@@ -53,7 +57,7 @@ def abrir_desafio_e_ler_planilha():
 
 def preencher_campos(data_frame):
     try:
-        p.press('pagedown')
+        p.press('pagedown', 3)
         p.sleep(1)
         cont = 0
 
@@ -67,54 +71,97 @@ def preencher_campos(data_frame):
                 p.sleep(1)
                 p.click(start)
                 print('clicou start')
+                p.sleep(1)
+                p.press('f11')
                 break
-
 
         # Preencher campos
         for linha in data_frame.itertuples():
             for c in range(1, 8):
-                p.press('pagedown')
                 p.sleep(0.5)
 
                 cont = 0 
                 while (cont < 10):
+                    x = None
+                    y = None
+                    p.press('pageup', 3)
+                    p.sleep(0.3)
                     try:
-                        print(f'buscando imagem campo{c}_{cont}.png')
                         cont += 1
-                        p.sleep(1)
                         x, y = p.locateCenterOnScreen(f'C:\\Curso02_github\\desafio_Final\\campos\\campo{c}_{cont}.png', confidence=0.9)
                         if (x is not None) or (y is not None):
-                            print(f'\nACHOU imagem campo{c}_{cont}')
                             p.click(x, (y+30))
-                            p.sleep(2)
+                            p.sleep(0.1)
                             print(f'CLICOU imagem campo{c}_{cont}')
-                            p.write(str(linha[c+1]))
-                            print(f'DIGITOU {linha[c+1]}\n')
+                            p.write(str(linha[c]))
                             break
                     except:
-                        exc_type, error, line = sys.exc_info()
-                        error_msg = f'ERROR: {error}\nCLASS: {exc_type}\nFUNC: {sys._getframe().f_code.co_name}\nLINE:  {line.tb_lineno}\n'
-                        p.alert(error_msg)
-                        p.sleep(0.5)
+                        p.sleep(0.3)
+
+                    p.press('pagedown', 3)
+                    p.sleep(0.3)
+                    try:
+                        x, y = p.locateCenterOnScreen(f'C:\\Curso02_github\\desafio_Final\\campos\\campo{c}_{cont}.png', confidence=0.9)
+                        if (x is not None) or (y is not None):
+                            p.click(x, (y+30))
+                            p.sleep(0.1)
+                            print(f'CLICOU imagem campo{c}_{cont}')
+                            p.write(str(linha[c]))
+                            break
+                    except:
+                        p.sleep(0.3)
             
             find_buttom = 0
             while (find_buttom < 15):
+                print('buscando submit')
                 find_buttom += 1
                 bt = p.locateCenterOnScreen(f'C:\\Curso02_github\\desafio_Final\\botao_submit.png', confidence=0.9)
                 if (bt is not None):
                     p.click(bt)
                     print('clicou submit')
                     break
+                p.press('pagedown', 3)
+
+        # Tirar print
+        my_screenshot = p.screenshot()
+        my_screenshot.save(r'C:\\Curso02_github\\desafio_Final\\screenshots\\ultimo_print.png')
+        print('salvou screenshot')
+
     except:
         exc_type, error, line = sys.exc_info()
         print(f'ERROR: {error}\nCLASS: {exc_type}\nFUNC: {sys._getframe().f_code.co_name}\nLINE:  {line.tb_lineno}\n')    
-                
-def main():
-    data_frame = abrir_desafio_e_ler_planilha()
-    preencher_campos(data_frame)
+
+
+def desvaziar_downloads(full_path):
+    print(f'esvaziando caminho: {full_path}')
+    arq_lista = os.listdir(full_path)
+    print(f'existem {len(arq_lista)} arquivos em downloads')
+    for arquivo in arq_lista:
+        if arquivo.endswith('xls') or arquivo.endswith('xlsx'):
+            os.remove(f'{full_path}\\{arquivo}')
+            print(f'removeu {full_path}\\{arquivo}')
+        else:
+            print(f'arquivo {arquivo} nao e excel')
+
+  
+def bot_atividade_001():
+    try:
+        path = "~\\Downloads\\"
+        full_path = os.path.expanduser(path)
+        desvaziar_downloads(full_path)
+        data_frame = abrir_desafio_e_ler_planilha()
+        preencher_campos(data_frame)
+        desvaziar_downloads(full_path)
+        #if success:
+            #mandar email SUCESSO 
+    except:
+        exc_type, error, line = sys.exc_info()
+        print(f'ERROR: {error}\nCLASS: {exc_type}\nFUNC: {sys._getframe().f_code.co_name}\nLINE:  {line.tb_lineno}\n')    
+        # mandar email com o erro => nós 
+        
 
 
 if (__name__ == '__main__'):
-    o.system('taskkill /f /im chrome.exe')
-    o.system('cls')
-    main()
+    os.system('taskkill /f /im chrome.exe')
+    os.system('cls')
+    bot_atividade_001()
