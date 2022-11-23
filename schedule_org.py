98aa1b11371ba03411f88b0206a3d046c52e52be
Felipe Sanches <fsanches.0502@gmail.com>
2022-11-23 17:10:46 -0300

    2022-11-23 17:10:44.829284
    teste

--- schedule_org.py
+++ schedule_org.py
@@ -4,14 +4,11 @@ from atividade_09_automatizar_github.robo_09 import bot_09
 from atividade_07_automatizar_email.robo_07 import enviar_email
 
 
-try:
-    print('iniciou...')
-    #SEMPRE ALTERAR O NOME DA PASTE SCHEDULE
-    schedule.every().day.at("16:55").do(bot_09())
-    enviar_email('fsanches.0502@gmail.com','commitado com sucesso')
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-except IndexError as e:
-    #envia um email para alguém
-    enviar_email('fsanches.0502@gmail.com',f'Erro =  {e}')
+
+print('iniciou...')
+#SEMPRE ALTERAR O NOME DA PASTE SCHEDULE
+schedule.every().day.at("16:55").do(bot_09())
+enviar_email('fsanches.0502@gmail.com','commitado com sucesso')
+while True:
+    schedule.run_pending()
+    time.sleep(1)
