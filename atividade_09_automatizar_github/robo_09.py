d28de1c840d7af7b65a66cb1f5cf4b6e7392cff6
Felipe Sanches <fsanches.0502@gmail.com>
2022-11-23 16:11:58 -0300

    2022-11-23 16:11:56.123352
    teste

--- atividade_09_automatizar_github/robo_09.py
+++ atividade_09_automatizar_github/robo_09.py
@@ -2,11 +2,47 @@ import pyautogui as p
 import os 
 from keyboard import write
 from time import sleep
+import time
+import datetime
 
-p.hotkey('win','r')
-write('C:\\Users\\Ana Paula\\AppData\\Local\\GitHubDesktop\\GitHubDesktop.exe')
-p.press('enter')
-sleep(2)
-x1,y1 = p.locateCenterOnScreen('C:\\Curso02_github\\atividade_09_automatizar_github\\ima\\listar_repositorio.png')
-p.moveTo(x1,y1)
-p.click()
+
+
+def abrir_github():
+    p.hotkey('win','r')
+    write('C:\\Users\\Ana Paula\\AppData\\Local\\GitHubDesktop\\GitHubDesktop.exe')
+    p.press('enter')
+    sleep(1.5)
+
+
+def escolher_pagina_e_subir_arquivos():
+    x1,y1 = p.locateCenterOnScreen('C:\\Curso02_github\\atividade_09_automatizar_github\\ima\\listar_repositorio.png')
+    p.moveTo(x1,y1)
+    p.click()
+    x1,y1 = p.locateCenterOnScreen('C:\\Curso02_github\\atividade_09_automatizar_github\\ima\\Curso02_github.png')
+    p.moveTo(x1,y1)
+    p.click()
+    x1,y1 = p.locateCenterOnScreen('C:\\Curso02_github\\atividade_09_automatizar_github\\ima\\titulo.png')
+    p.moveTo(x1,y1)
+    p.click()
+    write(f'{datetime.datetime.now()}')
+    x1,y1 = p.locateCenterOnScreen('C:\\Curso02_github\\atividade_09_automatizar_github\\ima\\descriptions.png')
+    p.moveTo(x1,y1)
+    p.click()
+    write(f'teste')
+    p.hotkey('ctrl','p')
+    x1,y1 = p.locateCenterOnScreen('C:\\Curso02_github\\atividade_09_automatizar_github\\ima\\commit_to_main.png')
+    p.moveTo(x1,y1,duration=1)
+    p.click()
+    p.hotkey('ctrl','p')
+
+
+def bot_09():
+    os.system('taskkill /f /im GitHubDesktop.exe')
+    os.system('cls')
+    abrir_github()
+    escolher_pagina_e_subir_arquivos()
+
+if __name__ == '__main__':
+    os.system('cls')
+    bot_09()
+    print('robô executado com sucesso')
